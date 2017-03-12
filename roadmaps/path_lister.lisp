a096cd145da0c5eb3a72c3a335ab19e546f213b6
Виталий Гузеев <guzeev@smertdec.ru>
2017-03-12 16:33:39 +0300

    Just status update

--- roadmaps/path_lister.lisp
+++ roadmaps/path_lister.lisp
@@ -2,5 +2,6 @@
 
 ;Returns all paths from first arg to second via map from third.
 ;Sorting by length while construct.
+;append to list only correct. use high order recursion
 (defun generate_paths (List)
 )
